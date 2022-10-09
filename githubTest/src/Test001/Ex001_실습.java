0d492375073afcddc55421f6d5c6ab3ae557ed87
Lee Soyeon <data.sy.2@gmail.com>
2022-10-09 11:51:39 +0900

    Test001
    클래스파일로 만들었던 이론내용들을 md파일로 만들 때 어떤지 확인

---
+++ githubTest/src/Test001/Ex001_실습.java
@@ -0,0 +1,5 @@
+package Test001;
+
+public class Ex001_실습 {
+
+}
---
+++ githubTest/src/Test001/Ex003_그다음실습.java
@@ -0,0 +1,19 @@
+package Test001;
+
+public class Ex003_그다음실습 {
+    public static void main(String[] args) {
+        // 곱셈표
+        for (int i=1; i<=9; i++) {
+            for (int j=1; j<=9; j++) {
+                System.out.printf("%3d", i*j);
+            }
+            System.out.println();
+        }
+
+        // printf
+        // System.out.printf("출력 서식",출력할 내용);
+        // %3d는 결과물을 3자리로 출력하라는 말
+
+    }
+
+}
